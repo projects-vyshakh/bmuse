d1dd01201535d09fba214202ca33542ac0e54615
vys-vital <vyshakh@vtldesign.com>
2021-03-16 03:08:33 +0530

    enabled the seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
          $this->call(UserSeeder::class);
 
-        //$this->call(PostSeeder::class);
-        //$this->call(TagSeeder::class);
+        $this->call(PostSeeder::class);
+        $this->call(TagSeeder::class);
     }
 }
